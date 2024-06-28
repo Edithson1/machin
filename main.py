dd8d750e7cbaa02f541d241236df6ce83e638f05
Edithson Aybar <152218004+Edithson1@users.noreply.github.com>
2024-06-28 03:05:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import streamlit as st
+
+# Título de la aplicación
+st.title('Mi primera aplicación de Streamlit')
+
+# Slider para seleccionar un número
+number = st.slider('Selecciona un número', 0, 100, 50)
+
+# Mostrar el número seleccionado
+st.write('El número seleccionado es:', number)
 
