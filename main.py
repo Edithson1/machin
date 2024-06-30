7450da3c42f540f27e1f28d454752adb47bbbc7a
Edithson Aybar <152218004+Edithson1@users.noreply.github.com>
2024-06-30 17:07:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import gdown
 from PIL import Image
 from io import BytesIO
 from tensorflow.keras.models import load_model
@@ -10,8 +11,9 @@ from contextlib import contextmanager
 import tqdm
 import streamlit as st
 
+
 # URL base de la API de GitHub
-api_url = 'https://github.com/Edithson1/machin/tree/main/test'
+api_url = 'https://api.github.com/repos/Edithson1/machin/contents/test'
 
 # Directorio local para almacenar las imágenes descargadas
 local_dir = 'imagenes_descargadas'
@@ -21,8 +23,16 @@ os.makedirs(local_dir, exist_ok=True)
 response = requests.get(api_url)
 files = response.json()
 
-# Filtrar solo las imágenes (asumiendo que las imágenes tienen extensiones conocidas)
-image_files = [file for file in files if file['name'].lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif'))]
+# Mostrar la respuesta de la API en Streamlit para depuración
+st.write("Respuesta de la API de GitHub:")
+st.write(files)
+
+# Comprobar si la respuesta es una lista
+if isinstance(files, list):
+    # Filtrar solo las imágenes (asumiendo que las imágenes tienen extensiones conocidas)
+    image_files = [file for file in files if 'name' in file and file['name'].lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif'))]
+else:
+    st.error("La respuesta de la API no es una lista. Verifique la URL y los permisos del repositorio.")
 
 # Descargar y guardar las imágenes
 for file in image_files:
@@ -37,24 +47,3 @@ model = load_model('model.keras')
 
 # Carpeta de las imágenes de prueba
 directorio_pruebas = local_dir
-
-# Función para cargar y preprocesar una imagen
-def cargar_y_preprocesar_imagen(ruta_imagen):
-    imagen = cv2.imread(ruta_imagen)
-    imagen = cv2.resize(imagen, (512, 512))
-    imagen = imagen.astype('float32') / 255.0
-    imagen = np.expand_dims(imagen, axis=-1)
-    imagen = np.expand_dims(imagen, axis=0)
-    return imagen
-
-# Context manager para suprimir la salida estándar
-@contextmanager
-def suppress_stdout():
-    with open(os.devnull, "w") as devnull:
-        old_stdout = sys.stdout
-        sys.stdout = devnull
-        try:
-            yield
-        finally:
-            sys.stdout = old_stdout
-
