d1c88c5e8bcc399004e08ccc9ff755c4670bbbd7
Edithson Aybar <152218004+Edithson1@users.noreply.github.com>
2024-06-30 16:37:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import os
 import requests
 from PIL import Image
 from io import BytesIO
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import pandas as pd
 import numpy as np
 import cv2
 from contextlib import contextmanager
 import tqdm
+import streamlit as st
 
 # URL base de la API de GitHub
 api_url = 'https://api.github.com/repos/Edithson1/machin/contents/test'
@@ -20,8 +21,16 @@ os.makedirs(local_dir, exist_ok=True)
 response = requests.get(api_url)
 files = response.json()
 
-# Filtrar solo las imágenes (asumiendo que las imágenes tienen extensiones conocidas)
-image_files = [file for file in files if file['name'].lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif'))]
+# Mostrar la respuesta de la API en Streamlit para depuración
+st.write("Respuesta de la API de GitHub:")
+st.write(files)
+
+# Comprobar si la respuesta es una lista
+if isinstance(files, list):
+    # Filtrar solo las imágenes (asumiendo que las imágenes tienen extensiones conocidas)
+    image_files = [file for file in files if 'name' in file and file['name'].lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif'))]
+else:
+    st.error("La respuesta de la API no es una lista. Verifique la URL y los permisos del repositorio.")
 
 # Descargar y guardar las imágenes
 for file in image_files:
