1ba4eb4dd784bfac3a00cebb023694e500b6f13a
Edithson Aybar <152218004+Edithson1@users.noreply.github.com>
2024-06-30 17:59:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 
 # Carpeta de las imágenes de prueba
-directorio_pruebas = 'https://github.com/Edithson1/machin/tree/main/test'
+directorio_pruebas = '/test'
 
 # Verificar si el directorio existe antes de listar archivos
 if not os.path.exists(directorio_pruebas):
