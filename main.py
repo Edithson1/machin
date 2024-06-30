1ff184412b922454bae95f7ab7ad2c65bf3273b9
Edithson Aybar <152218004+Edithson1@users.noreply.github.com>
2024-06-30 16:09:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import streamlit as st
+import os
+from PIL import Image
 
-col1, col2 = st.columns([2,1])
+# Configurar directorio de almacenamiento
+DIRECTORIO_ALMACENAMIENTO = 'imagenes_subidas'
 
-col1.markdown(" # Bienvenidos a mi app! ")
-col1.markdown(" Aquí va algo de información. ")
+# Crear el directorio si no existe
+if not os.path.exists(DIRECTORIO_ALMACENAMIENTO):
+    os.makedirs(DIRECTORIO_ALMACENAMIENTO)
 
-datos = col2.file_uploader(" Carga aquí tu archivo de datos.csv ")
+# Layout de Streamlit
+col1, col2 = st.columns([2, 1])
 
+col1.markdown("# Bienvenidos a mi app!")
+col1.markdown("Aquí va algo de información.")
 
-st.title('Mi primera aplicación de Streamlit')
+# Cargar archivos
+archivos_subidos = col2.file_uploader("Carga aquí tus archivos", accept_multiple_files=True)
 
-number = st.slider('Selecciona un número', 0, 100, 50)
+if archivos_subidos:
+    for archivo in archivos_subidos:
+        # Leer imagen
+        img = Image.open(archivo)
+        
+        # Mostrar imagen
+        col1.image(img, caption=archivo.name)
 
-st.write('El número seleccionado es:', number)
-
+        # Guardar imagen
+        ruta_archivo = os.path.join(DIRECTORIO_ALMACENAMIENTO, archivo.name)
+        img.save(ruta_archivo)
+        
+    st.success("Archivos subidos y almacenados con éxito.")
