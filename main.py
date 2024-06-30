879ebb668dd9459141262c061a18e608fe771667
Edithson Aybar <152218004+Edithson1@users.noreply.github.com>
2024-06-30 16:07:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,10 @@ col1.markdown(" Aquí va algo de información. ")
 
 datos = col2.file_uploader(" Carga aquí tu archivo de datos.csv ")
 
-foto = col2.camera_input(" Tomar foto ")
-col2.success(" Tu foto se cargo correctamente! ")
 
-# streamlit run app.py
-# Título de la aplicación
 st.title('Mi primera aplicación de Streamlit')
 
-# Slider para seleccionar un número
 number = st.slider('Selecciona un número', 0, 100, 50)
 
-# Mostrar el número seleccionado
 st.write('El número seleccionado es:', number)
 
