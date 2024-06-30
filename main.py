30e420b50002db8f2dd27e727458f3c0aa44f555
Edithson Aybar <152218004+Edithson1@users.noreply.github.com>
2024-06-30 17:51:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import os
-from keras import models
-import numpy as np
-import tensorflow as tf
-import sys
-from contextlib import contextmanager
-import tqdm
-
+import streamlit as st
 
 # Carpeta de las imágenes de prueba
 directorio_pruebas = '/kaggle/working/upch-intro-ml/test/test'
 
-# Lista todos los archivos en el directorio de pruebas
-archivos = os.listdir(directorio_pruebas)
-
-# Mostrar cada imagen en Streamlit
-for archivo in archivos:
-    if archivo.endswith('.jpg') or archivo.endswith('.png'):
-        imagen_path = os.path.join(directorio_pruebas, archivo)
-        st.image(imagen_path, caption=archivo, use_column_width=True)
+# Verificar si el directorio existe antes de listar archivos
+if not os.path.exists(directorio_pruebas):
+    st.error(f"El directorio {directorio_pruebas} no existe. Verifica la ruta.")
+else:
+    # Lista todos los archivos en el directorio de pruebas
+    archivos = os.listdir(directorio_pruebas)
+    
+    # Mostrar cada imagen en Streamlit
+    for archivo in archivos:
+        if archivo.endswith('.jpg') or archivo.endswith('.png'):
+            imagen_path = os.path.join(directorio_pruebas, archivo)
+            st.image(imagen_path, caption=archivo, use_column_width=True)
