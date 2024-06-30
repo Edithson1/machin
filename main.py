06a6f47c2035a293478247f8671c69f9553e93b4
Edithson Aybar <152218004+Edithson1@users.noreply.github.com>
2024-06-30 18:56:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import os
 import streamlit as st
+import requests
+from tensorflow.keras.models import load_model
 
 # Carpeta de las imágenes de prueba
 directorio_pruebas = 'test'
+
+# URL pública de descarga desde Google Drive
+url = 'https://drive.google.com/uc?id=1GHc4_s-WtwA_04Pa2luZLP3sIy7akBIq'
+
+# Nombre del archivo local donde se guardará el modelo
+archivo_modelo = 'modelo.keras'
 
 # Verificar si el directorio existe antes de listar archivos
 if not os.path.exists(directorio_pruebas):
@@ -43,6 +51,29 @@ else:
             finally:
                 sys.stdout = old_stdout
 
+    # Función para descargar el archivo desde Google Drive
+    def descargar_desde_google_drive(url, archivo_destino):
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            with open(archivo_destino, 'wb') as f:
+                for chunk in response.iter_content(1024):
+                    f.write(chunk)
+            return True
+        else:
+            return False
+    
+    # Descargar el archivo del modelo desde Google Drive si no está localmente
+    if not os.path.exists(archivo_modelo):
+        if descargar_desde_google_drive(url, archivo_modelo):
+            st.write(f"Archivo descargado correctamente como {archivo_modelo}")
+        else:
+            st.error("Error al descargar el archivo desde Google Drive. Verifica la URL.")
+    
+    # Cargar el modelo desde el archivo descargado
+    modelo = load_model(archivo_modelo)
+    
+    # Ejemplo de uso del modelo en Streamlit
+    st.write("Modelo cargado correctamente. Puedes comenzar a hacer predicciones.")
     
     # Imprimir las rutas de las imágenes
     if rutas_imagenes:
