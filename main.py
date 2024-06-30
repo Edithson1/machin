5199cc1af46d84719469a0f9ae1d22144c823f9c
Edithson Aybar <152218004+Edithson1@users.noreply.github.com>
2024-06-30 14:38:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
+col1, col2 = st.columns([2,1])
+
+col1.markdown(" # Bienvenidos a mi app! ")
+col1.markdown(" Aquí va algo de información. ")
+
+datos = col2.file_uploader(" Carga aquí tu archivo de datos.csv ")
+
+foto = col2.camera_input(" Tomar foto ")
+col2.success(" Tu foto se cargo correctamente! ")
+
+# streamlit run app.py
 # Título de la aplicación
 st.title('Mi primera aplicación de Streamlit')
 
