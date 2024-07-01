092eb162588fca59471f90efbe0a2cfc54de9ab4
Edithson Aybar <152218004+Edithson1@users.noreply.github.com>
2024-06-30 21:58:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,19 @@ if uploaded_file is not None:
 # Carpeta de las imágenes de prueba
 directorio_pruebas = 'test'
 
+# Función para cargar y preprocesar una imagen
+def cargar_y_preprocesar_imagen(ruta_imagen):
+    imagen = cv2.imread(ruta_imagen)
+    imagen = cv2.resize(imagen, (512, 512))
+    imagen = imagen.astype('float32') / 255.0
+    imagen = np.expand_dims(imagen, axis=-1)
+    imagen = np.expand_dims(imagen, axis=0)
+    
+    return imagen
+
+    # Cargar el modelo
+modelo = load_model('modelo_eficiente1.keras')
+
 
 # Verificar si el directorio existe antes de listar archivos
 if not os.path.exists(directorio_pruebas):
@@ -34,38 +47,23 @@ else:
             imagen_path = os.path.join(directorio_pruebas, archivo)
             rutas_imagenes.append(imagen_path)
 
-    
-    # Función para cargar y preprocesar una imagen
-    def cargar_y_preprocesar_imagen(ruta_imagen):
-        imagen = cv2.imread(ruta_imagen)
-        imagen = cv2.resize(imagen, (512, 512))
-        imagen = imagen.astype('float32') / 255.0
-        imagen = np.expand_dims(imagen, axis=-1)
-        imagen = np.expand_dims(imagen, axis=0)
-    
-        return imagen
-
-    # Cargar el modelo
-    modelo = load_model('modelo_eficiente1.keras')
-    st.write("Modelo cargado correctamente.")
-
-    # Dataframe para almacenar nombres de imágenes y predicciones
-    resultados = []
-    
     # Procesar y predecir para cada imagen
-    st.write(len(rutas_imagenes))
-    if rutas_imagenes:
-        for ruta_imagen in rutas_imagenes:
-            nombre_imagen = os.path.basename(ruta_imagen)
-            imagen_procesada = cargar_y_preprocesar_imagen(ruta_imagen)
-            prediccion = modelo.predict(imagen_procesada)
-            if prediccion[0][0] >= 0.5:
-                resultado = 1
-            else:
-                resultado = 0
-            resultados.append({'Nombre de la imagen': nombre_imagen, 'score': resultado})
-        df_resultados = pd.DataFrame(resultados)
-        st.write("Resultados de las predicciones:")
-        st.dataframe(df_resultados)
-    else:
-        st.write("No se encontraron imágenes en el directorio especificado.")
+    st.write("Presione este boton para predecir la actividad")
+    if st.button('Presionar aquí'):
+        resultados = []
+        st.write(len(rutas_imagenes))
+        if rutas_imagenes:
+            for ruta_imagen in rutas_imagenes:
+                nombre_imagen = os.path.basename(ruta_imagen)
+                imagen_procesada = cargar_y_preprocesar_imagen(ruta_imagen)
+                prediccion = modelo.predict(imagen_procesada)
+                if prediccion[0][0] >= 0.5:
+                    resultado = 1
+                else:
+                    resultado = 0
+                resultados.append({'Nombre de la imagen': nombre_imagen, 'score': resultado})
+            df_resultados = pd.DataFrame(resultados)
+            st.write("Resultados de las predicciones:")
+            st.dataframe(df_resultados)
+        else:
+            st.write("No se encontraron imágenes en el directorio especificado.")
