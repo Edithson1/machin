7e0049f4e685ec726d7a5dd0a5f4a994d7a6a4a6
Edithson Aybar <152218004+Edithson1@users.noreply.github.com>
2024-06-30 22:51:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ modelo = load_model('modelo_eficiente1.keras')
 directorio_pruebas = 'test'
 
 # Función para cargar y preprocesar una imagen
-def cargar_y_preprocesar_imagen(ruta_imagen):
-    imagen = cv2.resize(imagen, (512, 512))
+def cargar_y_preprocesar_imagen(image):
+    imagen = cv2.resize(image, (512, 512))
     imagen = imagen.astype('float32') / 255.0
     imagen = np.expand_dims(imagen, axis=-1)
     imagen = np.expand_dims(imagen, axis=0)
